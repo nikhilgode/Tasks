20d87fd29700626fb36d0aaa4597057408ecc27a
abhaymishra <abhaymishra@nimapinfotech.com>
2024-01-31 11:37:19 +0530

    Day 2 start

---
+++ Day 2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Day_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ListPractice l1 = new ListPractice();
+
+            int[] x = { 1, 2, 3 };
+
+            l1.addinlist(1);
+            l1.addinlist(2);
+            l1.addinlist(3);
+            l1.printlist();
+            l1.addinlist(1);
+            l1.addrangeinlisr(x);
+            l1.printlist();
+            l1.removefromlist(1);
+
+
+
+
+           
+
+
+        }
+    }
+}
