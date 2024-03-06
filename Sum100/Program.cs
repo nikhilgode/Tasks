7df288bff9af384c3c61455e3767f7465837236e
nikhilgode <nikhilgode@nimapinfotech.com>
2024-03-06 17:45:56 +0530

    logical question

---
+++ Sum100/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Sum100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 80, 60, 10, 50, 30, 100, 0, 50 };
+            sum100(n);
+        }
+
+        public static  void sum100(int[] n)
+        {
+           for (int i = 0; i < n.Length-1; i++)
+            {
+                for(int j =i+1; j < n.Length; j++)
+                {
+                    if (n[i] + n[j]==100)
+                    Console.WriteLine($"Pair is   {n[i]}   and   {n[j]}");
+                }
+            }
+        }
+    }
+}
